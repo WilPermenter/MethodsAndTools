26c3efb3f3639434b23521590d003e6519076ed0
MarkBWooten <109125669+MarkBWooten@users.noreply.github.com>
2022-07-29 03:08:40 -0500

    Add files via upload

---
+++ user.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <string>
+
+using std::string;
+
+class User {
+private:
+    string username;
+    string password;
+    string streetAddress;
+    string state;
+    string city;
+    string paymentInfo;
+
+    int zip;
+    int cartID;
+
+    bool isLoggedIn;
+
+public:
+    User(string username, string password, string address, string state,
+        string city, int zip);
+
+    string getUsername();
+    string getPassword();
+    string getStreetAddress();
+    string getState();
+    string getCity();
+    string getPaymentInfo();
+    int getZip();
+    int getCartID();
+
+    void setUsername(string username);
+    void setPassword(string password);
+    void setStreetAddress(string address);
+    void setState(string state);
+    void setCity(string city);
+    void setPaymentInfo(string payment);
+    void setZip(int zip);
+    void setCartID(int cartID);   
+    
+    void assignCart();
+    void login(string pwd);
+
+    void deleteUser();
+
+};
