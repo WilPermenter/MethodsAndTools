b057054f962c6f70471ec43fcfa363731fc2bda9
MarkBWooten <109125669+MarkBWooten@users.noreply.github.com>
2022-08-02 14:02:51 -0500

    Update user.cpp

--- user.cpp
+++ user.cpp
@@ -1,15 +1,27 @@
 #include "user.h"
 
 User::User() {
-    username = "N/A";
-    password = "N/A";
-    streetAddress = "N/A";
-    state = "N/A";
-    city = "N/A";
-    paymentInfo = "N/A";
+    username = "";
+    password = "";
+    streetAddress = "";
+    state = "";
+    city = "";
+    paymentInfo = "";
     zip = -1;
     cartID = -1;
-    loggedIn = false;
+}
+User::User(string username, string password, string streetAddress, string state, 
+    string city, string paymentInfo, int zip, int cartID) {
+    
+    this->username = username;
+    this->password = password; 
+    this->streetAddress = streetAddress;
+    this->state = state;
+    this->city = city;
+    this->paymentInfo = paymentInfo;
+    this->zip = zip;
+    this->cartID = cartID;
+
 }
 
 string User::getUsername() { return username; }
@@ -18,10 +30,45 @@ string User::getStreetAddress() { return streetAddress; }
 string User::getState() { return state; } 
 string User::getCity() { return city; }
 string User::getPaymentInfo() { return paymentInfo;}
+
 int User::getZip() { return zip; }
 int User::getCartID() { return cartID; }
 
-bool User::isLoggedIn() { 
-    if (loggedIn) { return true; }
-    else { return false;}
+bool User::isLoggedIn() { return loggedIn; }
+
+void User::setUsername(string username) { 
+    this->username = username; 
+}
+void User::setPassword(string password) {
+    this->password = password;
+}
+void User::setStreetAddress(string address) {
+    this->streetAddress = address;
+}
+void User::setState(string state) {
+    this->state = state;
+}
+void User::setCity(string city) {
+    this->city = city;
+}
+void User::setPaymentInfo(string paymentInfo) {
+    this->paymentInfo = paymentInfo;
+}
+
+void User::setZip(int zip) {
+    this->zip = zip;
+}
+void User::setCartID(int cartID) {
+    this->cartID = cartID;
+}
+
+bool User::login(string password) {
+    if (password == this->password) {
+        loggedIn = true;
+        return loggedIn;
+    }
+    else {
+        loggedIn = false;
+        return loggedIn;
+    }
 }
