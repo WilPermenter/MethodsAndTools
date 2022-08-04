4f67b07f6c7e2f36698ffd9765bdac7a687edfba
crystalskull17 <68124131+crystalskull17@users.noreply.github.com>
2022-08-03 20:09:28 -0400

    Update and rename orders.cpp to order.cpp

---
+++ src/order.cpp
@@ -0,0 +1,38 @@
+#include "headers/orders.h"
+
+Order::Order() {
+    //TO DO: Initialize item vector
+
+    orderID = -1;
+    userID = -1;
+    cartID = -1;
+    totalPrice = -1.00;
+}
+Order::Order(int orderID, int userID, int cartID, float totalPrice) {
+    //TO DO: Initialize item vector
+    
+    this->orderID = orderID;
+    this->userID = userID;
+    this->cartID = cartID;
+    this->totalPrice = totalPrice;
+    
+}
+//TO DO: set/get vector
+
+int Order::getOrderID() { return orderID; }
+int Order::getUserID() { return userID; }
+int Order::getCartID() { return cartID; }
+float Order::getTotalPrice() { return totalPrice; }
+
+void Order::setOrderID(int orderID) {
+    this->orderID = orderID;
+}
+void Order::setUserID(int userID) {
+    this->userID = userID;
+}
+void Order::setCartID(int cartID) {
+    this->cartID = cartID;
+}
+void Order::setTotalPrice(float totalPrice) {
+    this->totalPrice = totalPrice;
+}
