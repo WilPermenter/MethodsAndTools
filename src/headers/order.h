b8c9da205e37d1e0bbe119a1ded0bb886a3f2902
crystalskull17 <68124131+crystalskull17@users.noreply.github.com>
2022-08-03 20:09:35 -0400

    Update and rename orders.h to order.h

---
+++ src/headers/order.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "item.h"
+
+//PLEASE CHECK MY VECTOR STUFF
+
+class Order {
+
+    private: 
+        std::vector<Item> items;
+
+        int orderID;
+        int userID;
+        int cartID;
+        float totalPrice;
+
+
+    public:
+        Order();
+        Order(std::vector<Item> items, int userID, int cartID, float totalPrice);
+    
+        int getOrderID();
+        int getUserID();
+        int getCartID();
+        float getTotalPrice();
+    
+        void setOrderID(int orderID);
+        void setUserID(int userID);
+        void setCartID(int cartID);
+        void setTotalPrice(float totalPrice);
+};
