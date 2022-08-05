4759a8efbfb1ac9b25ca3d1c1a173f3be4eb4b1e
Mark B. Wooten <109125669+MarkBWooten@users.noreply.github.com>
2022-08-04 21:47:47 -0500

    Update DBInteract.cpp

--- src/DBInteract.cpp
+++ src/DBInteract.cpp
@@ -1,35 +1,28 @@
-#include "DBInteract.h"
+#include "headers/DBInteract.h"
 
 // constructor 
 DBInteract::DBInteract() {
-    // open (or create) the database 
-    rc = sqlite3_open("data.db", &db);
-
-    // add stuff that constructs the database on first boot
-    // could probably do this outside the program itself but
-    // i am having trouble figuring it out
+    // open the database 
+    rc = sqlite3_open("data/shoppingDB-methodstools.db", &db);
 }
 
 void DBInteract::addUser(User& user) {
 
-}
-void DBInteract::constructDB() {
-
     char* messageError;
 
-    // sql for user table
-    sql = "CREATE TABLE USER("
-                      "USERNAME       TEXT  PRIMARY KEY   NOT NULL, "
-                      "PASSWORD       TEXT         NOT NULL, "
-                      "ADDRESS        CHAR(50)     NOT NULL, "
-                      "STATE          CHAR(50)     NOT NULL, "
-                      "CITY           CHAR(50)     NOT NULL, "
-                      "ZIP            INT          NOT NULL, "
-                      "PAYMENTINFO    CHAR(50)     NOT NULL, "
-                      "CARTID         INT   FOREIGN KEY   NOT NULL ); ";
+    string name = user.getUsername();
+    string pwd = user.getPassword();
+    string add = user.getStreetAddress();
+    string state = user.getState();
+    string city = user.getCity();
+    string payment = user.getPaymentInfo();
+    string zip = to_string(user.getZip());
+    string cartID = to_string(user.getCartID());
+    
+    // no idea if this actually works
+    sql = "INSERT INTO USER VALUES(" + name + ", " + pwd + ", " + add + ", "
+        + state + ", " + city + ", " + payment + ", " + zip + ", " + cartID 
+        + ");";
 
-    // runs the sql and adds it to the database... probably
-    rc = sqlite3_exec(db, sql.c_str(), NULL, 0, &messageError);
-
-    // need to add sql for the rest of the tables 
+    sqlite3_exec(db, sql.c_str(), NULL, 0, &messageError);
 }
