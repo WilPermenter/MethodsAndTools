9b91d0f7f6c97dae53b4a0ad41407a139bb36ad3
crystalskull17 <68124131+crystalskull17@users.noreply.github.com>
2022-08-05 01:02:17 -0400

    Add files via upload

---
+++ cart.py
@@ -0,0 +1,13 @@
+class cart:
+    def __init__(self):
+        self.cartID = 0
+        self.items = "N/A"
+
+
+    def addToCart():
+
+
+    def removeFromCart():
+
+
+    def checkout():
---
+++ orders.py
@@ -0,0 +1,10 @@
+class Order:
+    def __init__(self):
+        self.orderID = 0
+        self.items = "N/A"
+        self.userID = 0
+        self.cartID = 0
+        self.totalPrice = 0
+
+
+    
